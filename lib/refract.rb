8f4c9dadfafb95ee7d79ee38d370af77110a4979
tuwukee <yulia.oletskaya@gmail.com>
2017-01-24 16:03:38 +0300

    Check if hash included

--- lib/refract.rb
+++ lib/refract.rb
@@ -3,13 +3,22 @@ class Refract
     fail(ArgumentError, 'Expected hash as the first argument') unless json.is_a?(Hash)
     fail(ArgumentError, 'Expected hash as the second argument') unless query.is_a?(Hash)
 
+    recursive_lookup(json.stringify_keys, query.stringify_keys)
+  end
+
+  private
+
+  def self.recursive_lookup(json, query)
     return [] unless json['content']
     return [] unless json['content'].is_a?(Array)
 
     result = json['content'].select do |element|
       query.map do |k, v|
-        # TODO: deep search
-        element[k] == v
+        if v.is_a?(Hash) && element[k].is_a?(Hash)
+          (v.to_a - element[k].to_a).empty?
+        else
+          element[k] == v
+        end
       end.all?
     end
 
